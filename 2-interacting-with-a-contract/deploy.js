59099472b52bcdab536a92874e9e85e629c8bba2
Michael Street <mi.street@gmail.com>
2018-10-03 01:05:06 -0400

    cleaned up the deploy script

--- 2-interacting-with-a-contract/deploy.js
+++ 2-interacting-with-a-contract/deploy.js
@@ -7,17 +7,17 @@ let provider = new Web3.providers.HttpProvider("http://127.0.0.1:8545");
 let web3 = new Web3(provider);
 
 // compile the contract
-
-let source = fs.readFileSync('./MyToken.sol', 'utf8');
-let compiledContract = solc.compile(source, 1);
-let abi = compiledContract.contracts[':MyToken'].interface;
-let bytecode = compiledContract.contracts[':MyToken'].bytecode;
-let gasEstimate = web3.eth.estimateGas({data: bytecode});
-let MyToken = web3.eth.contract(JSON.parse(abi));
+let source = fs.readFileSync('./MyToken.sol', 'utf8');  // read in the contract source code
+let compiledContract = solc.compile(source, 1);  // compile the contract
+let abi = JSON.parse(compiledContract.contracts[':MyToken'].interface); // extract and parse the contract's ABI
+let bytecode = compiledContract.contracts[':MyToken'].bytecode; // get the contract bytecode
+let gasEstimate = web3.eth.estimateGas({data: bytecode}); // see how much gas you'll need
+let MyToken = web3.eth.contract(abi);  // define the contract object
 
 // define the deploying account
 let from = web3.eth.accounts[0];
 
+// deploy a new instance of MyToken to the blockchain
 var myContractInstance = MyToken.new({
     from:from,
     data:bytecode,
